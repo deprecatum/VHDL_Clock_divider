c29e7e7b2de9b4a60cca968e11975b2b9d201512
deprecatum <33914332+deprecatum@users.noreply.github.com>
2018-04-11 22:28:26 +0100

    Update clock_div.vhd

--- clock_div.vhd
+++ clock_div.vhd
@@ -15,28 +15,28 @@ begin
 counter0 : process(clk, sw0) is
 --put some declarations with you want
 begin
-end counter0;
+end process counter0;
   
 counter1 : process(clk, sw1) is
 begin
-end counter1;
+end process counter1;
 
 counter2 : process(clk, sw1,sw2) is
 begin
-end counter2;
+end process counter2;
   
 counter3 : process(clk, sw3) is
 begin
-end counter3;
+end process counter3;
   
 counter4 : process(clk, sw1,sw3) is
 begin
-end counter4;
+end process counter4;
   
 
 counter5 : process(clk, sw2,sw3) is
 begin
-end counter5;
+end process counter5;
   
 --16 counters activated by the switches with some ands, that outputs a single signal with some ands
 
